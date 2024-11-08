59d0d496afb522c68216c6d27ce18854206f3854
vadimoyt <raidiyk@mail.ru>
2024-11-09 01:40:36 +0300

    Updated sorting and adding fake data

--- HouseHunt/catalog/tests/factories.py
+++ HouseHunt/catalog/tests/factories.py
@@ -2,16 +2,24 @@ from factory import fuzzy
 import factory
 from user.tests.factories import UserFactory
 
-from ..models import Building
+from ..models import Building, TypeOfBuilding
+
+
 
 class BuildingFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Building
 
     title = factory.Faker('word')
+    type_of_building = factory.LazyAttribute(
+        lambda o: TypeOfBuilding.objects.get(id=2)
+    )
+
     price = fuzzy.FuzzyFloat(0.01, 100000)
     description = factory.Faker('paragraph')
     image_path = factory.django.ImageField(filename='building.jpg')
     user_id = factory.SubFactory(UserFactory)
+    square = fuzzy.FuzzyInteger(1, 200)
+    year_of_construction = fuzzy.FuzzyInteger(1900,2024)
 
 
--- HouseHunt/catalog/views.py
+++ HouseHunt/catalog/views.py
@@ -65,6 +65,7 @@ def change_data(request, id):
 def sorted_building(request):
     type_of_building = request.GET.get('category')
     square = request.GET.get('square')
+    year = request.GET.get('year')
     buildings = Building.objects.all()
     if not type_of_building and not square:
         return redirect('index')
@@ -73,6 +74,9 @@ def sorted_building(request):
     if square:
         min_square, max_square = map(int, square.split('-'))
         buildings = buildings.filter(square__gte=min_square, square__lte=max_square)
+    if year:
+        min_year, max_year = map(int, year.split('-'))
+        buildings = buildings.filter(year_of_construction__gte=min_year, year_of_construction__lte=max_year)
     paginator = Paginator(buildings, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
